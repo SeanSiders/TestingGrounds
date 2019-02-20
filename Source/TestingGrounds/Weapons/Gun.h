ae999d75ab0610a5ec6cf665372d6cc875012f9a
Sean Siders <sean.siders@icloud.com>
2019-02-20 05:43:39 -0800

    Moved all gun c++ to an actor class (OnFire() not working)

---
+++ Source/TestingGrounds/Weapons/Gun.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Gun.generated.h"
+
+UCLASS()
+class TESTINGGROUNDS_API AGun : public AActor
+{
+	GENERATED_BODY()
+    
+    /** Gun mesh: 1st person view (seen only by self) */
+    UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+    class USkeletalMeshComponent* Gun;
+    
+    /** Location on gun mesh where projectiles should spawn. */
+    UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+    class USceneComponent* MuzzleLocation;
+	
+public:	
+	// Sets default values for this actor's properties
+	AGun();
+    
+    /** Gun muzzle's offset from the characters location */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
+    FVector GunOffset;
+    
+    /** Projectile class to spawn */
+    UPROPERTY(EditDefaultsOnly, Category=Projectile)
+    TSubclassOf<class ABallProjectile> ProjectileClass;
+    
+    /** Sound to play each time we fire */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
+    class USoundBase* FireSound;
+    
+    /** AnimMontage to play each time we fire */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+    class UAnimMontage* FireAnimation;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+    class UAnimInstance* AnimInstance;
+    
+    /** Fires a projectile. */
+    void OnFire();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
